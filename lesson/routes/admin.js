48a17d7c29b1c8d44861402da3a289a2a432026f
Nick Melms <nick@craftedagency.com>
2024-08-24 20:32:03 -0400

    404 page

--- lesson/routes/admin.js
+++ lesson/routes/admin.js
@@ -1,18 +1,14 @@
 const express = require("express");
+const path = require("path");
 
 const router = express.Router();
 
 router.get("/add-product", (req, res, next) => {
   console.log("in the middleware");
-  res.send(`
-        <form method="POST" action="/admin/add-product">
-            <input type="text" name="title">
-            <button type="submit">Add Product</button>
-        </form>`);
+  res.sendFile(path.join(__dirname, "../", "views", "add-product.html"));
 });
 
 router.post("/add-product", (req, res, next) => {
-  console.log("product ran");
   res.redirect("/");
   console.log(req.body);
 });
