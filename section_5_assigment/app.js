474ee61f69ddecbe8733e5564a1b7c8c62dbf2fa
Nick Melms <nick@craftedagency.com>
2024-08-24 21:30:29 -0400

    add routes to practice assigment

---
+++ section_5_assigment/app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const path = require("path");
+
+const indexRoutes = require("./routes/index");
+const userRoutes = require("./routes/users");
+
+const app = express();
+
+app.use("/", indexRoutes);
+
+app.use("/users", userRoutes);
+
+app.listen(3000);
