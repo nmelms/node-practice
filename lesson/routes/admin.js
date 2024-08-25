58e3c107017a1dd97adaf3fdfd7fa6465091ba8e
Nick Melms <nick@craftedagency.com>
2024-08-24 20:39:47 -0400

    styles

--- lesson/routes/admin.js
+++ lesson/routes/admin.js
@@ -2,10 +2,11 @@ const express = require("express");
 const path = require("path");
 
 const router = express.Router();
+const rootDir = require("../utils/path");
 
 router.get("/add-product", (req, res, next) => {
   console.log("in the middleware");
-  res.sendFile(path.join(__dirname, "../", "views", "add-product.html"));
+  res.sendFile(path.join(rootDir, "views", "add-product.html"));
 });
 
 router.post("/add-product", (req, res, next) => {
